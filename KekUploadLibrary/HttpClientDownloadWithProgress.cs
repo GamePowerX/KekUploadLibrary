5171cb5d1441dc7116a38a4018b5a0c01967a4a8
CraftingDragon007 <craftingdragon007@outlook.com>
2022-03-02 16:24:19 +0100

    changes

--- KekUploadLibrary/HttpClientDownloadWithProgress.cs
+++ KekUploadLibrary/HttpClientDownloadWithProgress.cs
@@ -5,11 +5,11 @@ public class HttpClientDownloadWithProgress : IDisposable
     private readonly string _downloadUrl;
     private readonly string _destinationFilePath;
 
-    private HttpClient _httpClient;
+    private HttpClient? _httpClient;
 
     public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage);
 
-    public event ProgressChangedHandler ProgressChanged;
+    public event ProgressChangedHandler? ProgressChanged;
 
     public HttpClientDownloadWithProgress(string downloadUrl, string destinationFilePath)
     {
